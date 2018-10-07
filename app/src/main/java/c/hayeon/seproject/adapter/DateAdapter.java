f8b83ae9f5b6a231c309b24757be4740d162fbb4
Hayeon Kim <hayeon.kim1379@gmail.com>
2018-10-08 03:26:54 +1100

    adapter + tv -> btn

---
+++ app/src/main/java/c/hayeon/seproject/adapter/DateAdapter.java
@@ -0,0 +1,38 @@
+package c.hayeon.seproject.adapter;
+
+import android.view.ViewGroup;
+
+import com.thoughtbot.expandablerecyclerview.ExpandableRecyclerViewAdapter;
+import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup;
+
+import java.util.List;
+
+import c.hayeon.seproject.viewholder.DateViewHolder;
+import c.hayeon.seproject.viewholder.TimeViewHolder;
+
+public class DateAdapter extends ExpandableRecyclerViewAdapter<DateViewHolder, TimeViewHolder> {
+
+    public DateAdapter(List<? extends ExpandableGroup> groups) {
+        super(groups);
+    }
+
+    @Override
+    public DateViewHolder onCreateGroupViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public TimeViewHolder onCreateChildViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindChildViewHolder(TimeViewHolder holder, int flatPosition, ExpandableGroup group, int childIndex) {
+
+    }
+
+    @Override
+    public void onBindGroupViewHolder(DateViewHolder holder, int flatPosition, ExpandableGroup group) {
+
+    }
+}
--- app/src/main/java/c/hayeon/seproject/viewholder/TimeViewHolder.java
+++ app/src/main/java/c/hayeon/seproject/viewholder/TimeViewHolder.java
@@ -1,13 +1,29 @@
 package c.hayeon.seproject.viewholder;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.thoughtbot.expandablerecyclerview.viewholders.ChildViewHolder;
 
+import c.hayeon.seproject.R;
+
 public class TimeViewHolder extends ChildViewHolder {
-    TextView nineTv;
+    Button nineBtn;
+    Button nineHalfBtn;
+    Button tenBtn;
+    Button tenHalfBtn;
+
     public TimeViewHolder(View itemView) {
         super(itemView);
+        nineBtn = itemView.findViewById(R.id.nineBtn);
+        nineHalfBtn = itemView.findViewById(R.id.nineHalfBtn);
+        tenBtn = itemView.findViewById(R.id.tenBtn);
+       tenHalfBtn = itemView.findViewById(R.id.tenHalfBtn);
+    }
+
+    public void setNineBtn(String time){
+
+        nineBtn.setText(time);
     }
 }
