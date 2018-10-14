6a32e1122e5bf797f60d52ff3f225bdec2e8feb4
Mikhail <mikhail.dsilva@live.com.au>
2018-10-14 16:47:54 +1100

    Booking appointment function is working

--- app/src/main/java/c/hayeon/seproject/model/Date.java
+++ app/src/main/java/c/hayeon/seproject/model/Date.java
@@ -6,7 +6,16 @@ import java.util.List;
 
 public class Date extends ExpandableGroup {
 
+    String date;
+
     public Date(String title, List<Time> items) {
+
         super(title, items);
+        this.date = title;
+
+    }
+
+    public String getDate(){
+        return date;
     }
 }
